db1755779af30263345178f1d2a314f7dc04e139
mouahrara <mouahrara@users.noreply.github.com>
2024-03-18 01:58:31 +0100

    Relocate Farm Animals 1.0.0

---
+++ Relocate Farm Animals/srcs/ModEntry.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using HarmonyLib;
+using StardewModdingAPI;
+using RelocateFarmAnimals.Patches;
+
+namespace RelocateFarmAnimals
+{
+	/// <summary>The mod entry point.</summary>
+	internal sealed class ModEntry : Mod
+	{
+		// Shared static helpers
+		internal static new IModHelper	Helper { get; private set; }
+		internal static new IMonitor	Monitor { get; private set; }
+		internal static new IManifest	ModManifest { get; private set; }
+
+		public override void Entry(IModHelper helper)
+		{
+			Helper = base.Helper;
+			Monitor = base.Monitor;
+			ModManifest = base.ModManifest;
+
+			// Load Harmony patches
+			try
+			{
+				var harmony = new Harmony(ModManifest.UniqueID);
+
+				// Apply menu patches
+				AnimalQueryMenuPatch.Apply(harmony);
+			}
+			catch (Exception e)
+			{
+				Monitor.Log($"Issue with Harmony patching: {e}", LogLevel.Error);
+				return;
+			}
+		}
+	}
+}
